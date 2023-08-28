d85844d37b04dbbf785b046a2c32d7a421128366
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-08-28 20:04:18 -0300

    Update retorno-de-funcao.js

--- funçoes/modulo-1/retorno-de-funcao.js
+++ funçoes/modulo-1/retorno-de-funcao.js
@@ -2,6 +2,38 @@
 //Retorna um valor 
 //Termina a funcão 
 
-function soma (a, b){
+function soma(a, b) {
     return a + b;
 }
+console.log(soma(5, 2));
+
+function soma2(a, b) {
+    console.log(a + b);
+}
+soma2(5, 2);
+
+function criaPessoa(nome, sobrenome) {
+    return { nome, sobrenome }
+}
+
+const p1 = criaPessoa('Edson', 'Lima');
+const p2 = {
+    nome: 'Edsu',
+    sobrenome: 'Teles'
+};
+console.log(typeof p1);
+//com o typeof ele mostra o tipo do conteudo, se é uma string, number, objeto ou array
+
+console.log(p2);
+
+//outro sem retorno
+
+function falaFrase(comeco) {
+    function falaResto(resto) {
+        return comeco + ' ' + resto;
+    }
+    return falaResto;
+}
+
+const olaMundo = falaFrase('Olá');
+console.log(olaMundo('Mundo'));
