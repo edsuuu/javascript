944ce892ae75f340e908e8b2a9ddae411c4274d3
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-09-03 16:08:09 -0300

    Create index.js

---
+++ arrays/index.js
@@ -0,0 +1,8 @@
+//                0       1         2   
+const nomes = ['edson', 'lima', 'silva'];
+
+nomes.push('eds')// para adicionar ao final do array 
+nomes.unshift('eds')// para adicionar ao começo do array
+//pop remove do final 
+  
+console.log(nomes);
