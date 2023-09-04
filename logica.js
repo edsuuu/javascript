1c4b3e77fd7b32712c2ca573689527fafb25be9d
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-09-04 16:14:09 -0300

    Create logica.js

---
+++ logica.js
@@ -0,0 +1,13 @@
+const num1 = Number(prompt('Qual o primeiro número ?'));
+const num2 = Number(prompt('Qual o segundo número ?'));
+
+
+function maiorNumero(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else {
+        return numero2;
+    }
+}
+
+console.log(maiorNumero);
