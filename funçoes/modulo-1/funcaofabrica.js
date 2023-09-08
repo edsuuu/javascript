690dfbb594b239091ebd43cf9c69982d415e5b5c
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-09-08 09:44:53 -0300

    att

--- funçoes/modulo-1/funcaofabrica.js
+++ funçoes/modulo-1/funcaofabrica.js
@@ -1,21 +1,35 @@
 // Factory function (Função fábrica)
 // Constructor function (Função construtora)
-function criaPessoa(nome, sobrenome){
-  return{
+function criaPessoa(nome, sobrenome, a, p) {
+  return {
     nome,
     sobrenome,
-    fala/*function pode colocar function para funcionar essa funcao*/(assunto){
-      return `${this.nome} esta ${assunto}`;
+    get nomeCompleto(){
+    return `${nome} ${sobrenome}`
+
+    },
+    //metodos sao criados e separados por virgulo 
+    fala/*function pode colocar function para funcionar essa funcao*/(assunto) {
+      return `${this.nome} está ${assunto}.`;
+    },
+
+    altura: a,
+    peso: p,
+        /*Getter --> GET */ get imc() {
+      const indice = this.peso / (this.altura ** 2);
+      return indice.toFixed(2);
     }
   };
 }
-const p1 = criaPessoa('edsu', 'li ma')
-const p2 = criaPessoa('teles', 'silva')
-console.log(p2.fala('Falando sobre o assunto'));
-
-    // Getter
+//factory function acima/função fabrica
 
 
-    // Setter
+const p1 = criaPessoa('Luiz', 'Otávio', 1.8, 80);
+const p2 = criaPessoa('João', 'Otávio', 1.90, 57);
+const p3 = criaPessoa('Junior', 'Otávio', 1.5, 110);
+
+console.log(p1.imc);
+console.log(p2.imc);
+console.log(p3.imc);
 
     // Getter
