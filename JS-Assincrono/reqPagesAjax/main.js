0a7671f21f928a87a7b456925bdd168ceb981f05
edsuu <111332935+edsuuu@users.noreply.github.com>
2024-03-08 12:50:59 -0300

    com fetch api

--- JS-Assincrono/reqPagesAjax/main.js
+++ JS-Assincrono/reqPagesAjax/main.js
@@ -1,21 +1,21 @@
-const request = obj => {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(obj.method, obj.url, true);
-        xhr.send();
+// const request = obj => {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(obj.method, obj.url, true);
+//         xhr.send();
 
-        xhr.addEventListener('load', () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                resolve(xhr.responseText);
+//         xhr.addEventListener('load', () => {
+//             if (xhr.status >= 200 && xhr.status < 300) {
+//                 resolve(xhr.responseText);
 
-            } else {
-                reject(xhr.statusText);
-            }
-        });
-    })
+//             } else {
+//                 reject(xhr.statusText);
+//             }
+//         });
+//     })
 
 
-};
+// };
 
 document.addEventListener('click', e => {
     const el = e.target;
@@ -30,16 +30,14 @@ document.addEventListener('click', e => {
 });
 
 async function carregarPagina(el) {
-    const href = el.getAttribute('href');
-
-    const objConfig = {
-        method: 'GET',
-        url: href,
-    };
-
     try {
-        const response = await request(objConfig);
-        carregaResultado(response);
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if (response.status !== 200) throw new Error('Erro 404 Nosso ');
+
+        const html = await response.text();
+        carregaResultado(html);
     } catch (error) {
         console.log(error);
     }
@@ -49,3 +47,15 @@ function carregaResultado(response) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 };
+
+//PARA RETORNAR COM FETCH
+// fetch('pag4.html').then(resposta => {
+//     if (resposta.status !== 200) throw new Error('Erro 404 Nosso ');
+//     return resposta.text();
+// }).then(html => {
+//     console.log(html);
+
+// }).catch(e => {
+//     console.warn(e);
+//     console.error(e);
+// })
